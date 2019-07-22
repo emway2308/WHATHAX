4d9048a392b980d4dc71e96e163a8396c4e16101
emway2308 <49096814+emway2308@users.noreply.github.com>
2019-07-22 11:14:49 +0700

    Add files via upload

---
+++ WHATHAX.py
@@ -0,0 +1,102 @@
+#!/usr/bin/bash                                                                                                                              #Tools SadapWhatsApp
+#Sadap WhatsApp                                                                                                                              #By Dark4127
+#Jangan DiRecord Tod!!!                                                                                                                      #Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+     echo  $blue "<|======================================|>"
+     echo  $blue "<|            Tools SadapWhatsAPP       |>"
+     echo  $blue "<|                                      |>"
+     echo  $blue "<| Name       : Dark4127                |>"
+     echo  $blue "<| MyTeam     : Solo Player             |>"
+     echo  $blue "<| Contact    : 089613**********        |>"
+     echo  $blue "<| YouTube    : Hack Team               |>"
+     echo  $blue "<|======================================|>"
+sleep 3
+clear
+echo  $red "Tunggu Proses Penginstalannya"
+sleep 4
+clear                                                                                                                                        echo $red  "=============== $red MENU $green =================="
+   echo $blue "======================================="
+   echo $blue "|| $green 1. Gunakan Tools Ini Jika Perlu $blue ||"
+   echo $blue "|| $green 2. Jangan Di Salah Gunakan      $blue ||"
+   echo $blue "|| $green 3. Saya Tidak Bertanggung Jawab $blue ||"
+   echo $blue "|| $green 4. Saya Hanya Membuat           $blue ||"
+   echo $red  "||===================================||"
+   echo $blue "|| $green 1. Langsung Buka DataBase Chat  $blue ||"
+   echo $blue "|| $green 5. JANGAN LUPA JASA ADMIN       $blue ||"
+   echo $blue "||====================================="
+   echo $blue"╭──[Masukkan Pilihan Anda]>"
+      read -p"╰───────root@Dark4127=•>" Dark4127;
+if [ $Dark4127 = 1 ] || [ $Dark4127 = 01 ]
+then
+sleep 3
+echo $red "[+]Please Wait[#]"
+sleep 3
+clear
+sleep 2
+  echo $blue  "<|=====================================================|>"
+  echo $yellow  "||   Tools Ini Not Free (tidak gratis)                ||"
+  echo $blue  "<|=====================================================|>"
+  echo $red   "||   Kalo Anda Minat Hubungi Wa Saya Dan Bayar Dananya||"
+  echo $yellow"<|=====================================================|>"
+  echo $green "||                 089613616928                       ||"
+  echo $yellow"<|=====================================================|>"
+exit
+exit"the program"
+clear
+echo ".        ╔═══════════╗
+   ╔═╝███████████╚═╗
+╔╝███████████████╚╗
+║█████████████████║
+║█████████████████║
+║█████████████████║
+║█╔█████████████╗█║
+╚╦╝███▒▒███▒▒███╚╦╝
+╔╝██▒▒▒▒███▒▒▒▒██╚╗       "============================="
+║██▒▒▒▒▒███▒▒▒▒▒██║          "wait a minute"
+║██▒▒▒▒█████▒▒▒▒██║          "orang sabar disayang janda"
+                         "=============================="
+╚╗███████████████╔╝
+╔═╬══╦╝██▒█▒██╚╦══╝.▒..
+║█║══║█████████║　...▒.
+║█║══║█║██║██║█║　.▒..
+║█║══╚═╩══╩╦═╩═╩═╦╗▒.
+╔╝█╚══╦═╦══╦╩═╦═╦═╩╝
+╔╝█████║█║██║██║█║
+║██████║█████████║" | lolcat
+sleep 4
+clear
+sleep 3
+clear
+echo $red "MEMBUKA 7  CHT"
+sleep 4
+clear
+echo $green "Mama: Dek Jangan Lupa Ngambil Motornya Di Doorsmeer"
+echo $red   "iya mah"
+echo $green "Nisa: Put Lu Udh Siap Pr Mm"
+echo $red   "udh emng knapa Nis"
+echo $green "Nisa:  Gua Liat"
+echo $red   "ywdh kesia aja"
+echo $green "Yesha: Put Jangan Lupa Besok"
+echo $red   "iya selow wkwkwkwk"
+echo $green "Ferdi: Put Liat PR Mm"
+echo $red   "liat sama Nisa"
+echo $green "Ferdi: Ye Kata Dia Suruh Liat Lu"
+echo $green "Adit: Tengok Lah Pr Nya Wkw"
+echo $green "Adit: Jgn Pelit🤣"
+echo $red   "Liat Sama Ferdi Nisa Aja "
+echo $green "Adit: Jauh Rumah Orng Itu"
+echo $red   "liat Google Aja"
+echo $green "Adit: Owhh Oke"
+echo $green "Risky: P"
+echo $green "Risky: Nis Liat Pr🤣🤣"
+echo $red   "Haduh Cari Aja Di Google"
+echo $red   "Dari Tadi Bnyk Yang Minta Capek Gua"
+echo $green "Risky: Anjir Lah"
+echo $green "wahyu: P"
+fi
